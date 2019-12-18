f6593ccb69b709b6c36e71cb6066484c6708336b
Pere <peroc79@gmail.com>
2019-12-18 15:46:49 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,583 @@
+//africa_stats_index.php
+var index_json = {
+    "total_features": 38,
+    "min": 0.734777816,
+    "max": 0.649515193,
+    "avg_index": 0.483,
+    "features": [{
+        "iso_code": "CMR",
+        "country": "Cameroon",
+        "data": {
+            "index": 0.456154614
+        }
+    }, {
+        "iso_code": "CAF",
+        "country": "Central African Republic",
+        "data": {
+            "index": 0.380645658
+        }
+    }, {
+        "iso_code": "TCD",
+        "country": "Chad",
+        "data": {
+            "index": 0.447258219
+        }
+    }, {
+        "iso_code": "COG",
+        "country": "Congo",
+        "data": {
+            "index": 0.391694236
+        }
+    }, {
+        "iso_code": "DOM",
+        "country": "DR Congo",
+        "data": {
+            "index": 0.318942655
+        }
+    }, {
+        "iso_code": "BDI",
+        "country": "Burundi",
+        "data": {
+            "index": 0.434764697
+        }
+    }, {
+        "iso_code": "ERI",
+        "country": "Eritrea",
+        "data": {
+            "index": 0.382711144
+        }
+    }, {
+        "iso_code": "ETH",
+        "country": "Ethiopia",
+        "data": {
+            "index": 0.734777816
+        }
+    }, {
+        "iso_code": "KEN",
+        "country": "Kenya",
+        "data": {
+            "index": 0.67405969
+        }
+    }, {
+        "iso_code": "RWA",
+        "country": "Rwanda",
+        "data": {
+            "index": 0.557959294
+        }
+    }, {
+        "iso_code": "SOM",
+        "country": "Somalia",
+        "data": {
+            "index": 0.455446866
+        }
+    }, {
+        "iso_code": "ZAF",
+        "country": "South Sudan",
+        "data": {
+            "index": 0.649515193
+        }
+    }, {
+        "iso_code": "SDN",
+        "country": "Sudan",
+        "data": {
+            "index": 0.388844853
+        }
+    }, {
+        "iso_code": "UGA",
+        "country": "Uganda",
+        "data": {
+            "index": 0.545564444
+        }
+    }, {
+        "iso_code": "NGA",
+        "country": "Nigeria",
+        "data": {
+            "index": 0.487639368
+        }
+    }, {
+        "iso_code": "BEN",
+        "country": "Benin",
+        "data": {
+            "index": 0.495281225
+        }
+    }, {
+        "iso_code": "CHN",
+        "country": "Cote d'Ivoire",
+        "data": {
+            "index": 0.411991096
+        }
+    }, {
+        "iso_code": "GHA",
+        "country": "Ghana",
+        "data": {
+            "index": 0.357983066
+        }
+    }, {
+        "iso_code": "SEN",
+        "country": "Senegal",
+        "data": {
+            "index": 0.550508356
+        }
+    }, {
+        "iso_code": "TGO",
+        "country": "Togo",
+        "data": {
+            "index": 0.460188389
+        }
+    }, {
+        "iso_code": "BFA",
+        "country": "Burkina Faso",
+        "data": {
+            "index": 0.470095661
+        }
+    }, {
+        "iso_code": "GIN",
+        "country": "Guinea",
+        "data": {
+            "index": 0.407665387
+        }
+    }, {
+        "iso_code": "LBR",
+        "country": "Liberia",
+        "data": {
+            "index": 0.440405222
+        }
+    }, {
+        "iso_code": "MLI",
+        "country": "Mali",
+        "data": {
+            "index": 0.481829608
+        }
+    }, {
+        "iso_code": "MRT",
+        "country": "Mauritania",
+        "data": {
+            "index": 0.399201721
+        }
+    }, {
+        "iso_code": "NER",
+        "country": "Niger",
+        "data": {
+            "index": 0.432336716
+        }
+    }, {
+        "iso_code": "SLE",
+        "country": "Sierra Leone",
+        "data": {
+            "index": 0.401578388
+        }
+    }, {
+        "iso_code": "AGO",
+        "country": "Angola",
+        "data": {
+            "index": 0.388388028
+        }
+    }, {
+        "iso_code": "BWA",
+        "country": "Botswana",
+        "data": {
+            "index": 0.523427689
+        }
+    }, {
+        "iso_code": "LSO",
+        "country": "Lesotho",
+        "data": {
+            "index": 0.470977958
+        }
+    }, {
+        "iso_code": "MDG",
+        "country": "Madagascar",
+        "data": {
+            "index": 0.390995336
+        }
+    }, {
+        "iso_code": "MWI",
+        "country": "Malawi",
+        "data": {
+            "index": 0.419821806
+        }
+    }, {
+        "iso_code": "MOZ",
+        "country": "Mozambique",
+        "data": {
+            "index": 0.44636361
+        }
+    }, {
+        "iso_code": "NAM",
+        "country": "Namibia",
+        "data": {
+            "index": 0.449494803
+        }
+    }, {
+        "iso_code": "TJK",
+        "country": "Tanzania",
+        "data": {
+            "index": 0.456425418
+        }
+    }, {
+        "iso_code": "ZMB",
+        "country": "Zambia",
+        "data": {
+            "index": 0.43852689
+        }
+    }, {
+        "iso_code": "ZWE",
+        "country": "Zimbabwe",
+        "data": {
+            "index": 0.454049131
+        }
+    }, {
+        "iso_code": "ZAF",
+        "country": "South Africa",
+        "data": {
+            "index": 0.649515193
+        }
+    }]
+};
+//energy_index_level_1.php
+var level_1 = {
+    "total_features": 38,
+    "features": [{
+        "iso_code": "CMR",
+        "country": "Cameroon",
+        "data": {
+            "environmental": 0.02497596,
+            "social": 0.044861899,
+            "political": 0.090132004,
+            "financial": 0.296184752
+        }
+    }, {
+        "iso_code": "CAF",
+        "country": "Central African Republic",
+        "data": {
+            "environmental": 0.072358715,
+            "social": 0.073196342,
+            "political": 0.042167688,
+            "financial": 0.192922913
+        }
+    }, {
+        "iso_code": "TCD",
+        "country": "Chad",
+        "data": {
+            "environmental": 0.079291527,
+            "social": 0.076284898,
+            "political": 0.053216733,
+            "financial": 0.238465062
+        }
+    }, {
+        "iso_code": "COG",
+        "country": "Congo",
+        "data": {
+            "environmental": 0.028931763,
+            "social": 0.03997485,
+            "political": 0.062942815,
+            "financial": 0.259844808
+        }
+    }, {
+        "iso_code": "COD",
+        "country": "DR Congo",
+        "data": {
+            "environmental": 0.052784145,
+            "social": 0.062661034,
+            "political": 0.058686455,
+            "financial": 0.14481102
+        }
+    }, {
+        "iso_code": "BDI",
+        "country": "Burundi",
+        "data": {
+            "environmental": 0.0305685,
+            "social": 0.06490383,
+            "political": 0.058126654,
+            "financial": 0.281165713
+        }
+    }, {
+        "iso_code": "ERI",
+        "country": "Eritrea",
+        "data": {
+            "environmental": 0.067910058,
+            "social": 0.052160029,
+            "political": 0.058102403,
+            "financial": 0.204538654
+        }
+    }, {
+        "iso_code": "ETH",
+        "country": "Ethiopia",
+        "data": {
+            "environmental": 0.049018922,
+            "social": 0.054787527,
+            "political": 0.086086689,
+            "financial": 0.544884678
+        }
+    }, {
+        "iso_code": "KEN",
+        "country": "Kenya",
+        "data": {
+            "environmental": 0.048329107,
+            "social": 0.033952707,
+            "political": 0.119836647,
+            "financial": 0.47194123
+        }
+    }, {
+        "iso_code": "RWA",
+        "country": "Rwanda",
+        "data": {
+            "environmental": 0.027502613,
+            "social": 0.058582825,
+            "political": 0.170841319,
+            "financial": 0.301032538
+        }
+    }, {
+        "iso_code": "SOM",
+        "country": "Somalia",
+        "data": {
+            "environmental": 0.07204292,
+            "social": 0.063394651,
+            "political": 0.030587368,
+            "financial": 0.289421926
+        }
+    }, {
+        "iso_code": "ZAF",
+        "country": "South Sudan",
+        "data": {
+            "environmental": 0.036113567,
+            "social": 0.015145622,
+            "political": 0.139010664,
+            "financial": 0.459245341
+        }
+    }, {
+        "iso_code": "SDN",
+        "country": "Sudan",
+        "data": {
+            "environmental": 0.06655295,
+            "social": 0.043207489,
+            "political": 0.049989016,
+            "financial": 0.229095397
+        }
+    }, {
+        "iso_code": "UGA",
+        "country": "Uganda",
+        "data": {
+            "environmental": 0.027584758,
+            "social": 0.062567781,
+            "political": 0.123057315,
+            "financial": 0.33235459
+        }
+    }, {
+        "iso_code": "NGA",
+        "country": "Nigeria",
+        "data": {
+            "environmental": 0.027359741,
+            "social": 0.056188724,
+            "political": 0.118369083,
+            "financial": 0.285721819
+        }
+    }, {
+        "iso_code": "BEN",
+        "country": "Benin",
+        "data": {
+            "environmental": 0.028001019,
+            "social": 0.048991694,
+            "political": 0.125650972,
+            "financial": 0.292637539
+        }
+    }, {
+        "iso_code": "CHN",
+        "country": "Cote d'Ivoire",
+        "data": {
+            "environmental": 0.02234332,
+            "social": 0.046030298,
+            "political": 0.111163646,
+            "financial": 0.232453831
+        }
+    }, {
+        "iso_code": "GHA",
+        "country": "Ghana",
+        "data": {
+            "environmental": 0.020700128,
+            "social": 0.026976026,
+            "political": 0.135611901,
+            "financial": 0.174695012
+        }
+    }, {
+        "iso_code": "SEN",
+        "country": "Senegal",
+        "data": {
+            "environmental": 0.031069906,
+            "social": 0.04250162,
+            "political": 0.148956816,
+            "financial": 0.327980013
+        }
+    }, {
+        "iso_code": "TGO",
+        "country": "Togo",
+        "data": {
+            "environmental": 0.022966349,
+            "social": 0.049801048,
+            "political": 0.132793906,
+            "financial": 0.254627086
+        }
+    }, {
+        "iso_code": "BFA",
+        "country": "Burkina Faso",
+        "data": {
+            "environmental": 0.036913623,
+            "social": 0.058643787,
+            "political": 0.123491047,
+            "financial": 0.251047205
+        }
+    }, {
+        "iso_code": "GIN",
+        "country": "Guinea",
+        "data": {
+            "environmental": 0.042250322,
+            "social": 0.059528116,
+            "political": 0.098826854,
+            "financial": 0.207060094
+        }
+    }, {
+        "iso_code": "LBR",
+        "country": "Liberia",
+        "data": {
+            "environmental": 0.028394831,
+            "social": 0.060115315,
+            "political": 0.117923436,
+            "financial": 0.233971641
+        }
+    }, {
+        "iso_code": "MLI",
+        "country": "Mali",
+        "data": {
+            "environmental": 0.054573083,
+            "social": 0.061661927,
+            "political": 0.121038774,
+            "financial": 0.244555824
+        }
+    }, {
+        "iso_code": "MRT",
+        "country": "Mauritania",
+        "data": {
+            "environmental": 0.047573619,
+            "social": 0.049531258,
+            "political": 0.063855822,
+            "financial": 0.238241022
+        }
+    }, {
+        "iso_code": "NER",
+        "country": "Niger",
+        "data": {
+            "environmental": 0.055332591,
+            "social": 0.067463455,
+            "political": 0.086748853,
+            "financial": 0.222791817
+        }
+    }, {
+        "iso_code": "SLE",
+        "country": "Sierra Leone",
+        "data": {
+            "environmental": 0.038176736,
+            "social": 0.070585201,
+            "political": 0.114299386,
+            "financial": 0.178517065
+        }
+    }, {
+        "iso_code": "AGO",
+        "country": "Angola",
+        "data": {
+            "environmental": 0.041742937,
+            "social": 0.04943381,
+            "political": 0.049082704,
+            "financial": 0.248128577
+        }
+    }, {
+        "iso_code": "BWA",
+        "country": "Botswana",
+        "data": {
+            "environmental": 0.041996424,
+            "social": 0.033050911,
+            "political": 0.146664631,
+            "financial": 0.301715724
+        }
+    }, {
+        "iso_code": "LSO",
+        "country": "Lesotho",
+        "data": {
+            "environmental": 0.047283896,
+            "social": 0.052580929,
+            "political": 0.093622745,
+            "financial": 0.277490388
+        }
+    }, {
+        "iso_code": "MDG",
+        "country": "Madagascar",
+        "data": {
+            "environmental": 0.072258505,
+            "social": 0.067675286,
+            "political": 0.096441131,
+            "financial": 0.154620413
+        }
+    }, {
+        "iso_code": "MWI",
+        "country": "Malawi",
+        "data": {
+            "environmental": 0.043473663,
+            "social": 0.063043645,
+            "political": 0.104085939,
+            "financial": 0.209218559
+        }
+    }, {
+        "iso_code": "MOZ",
+        "country": "Mozambique",
+        "data": {
+            "environmental": 0.047458163,
+            "social": 0.0602668,
+            "political": 0.096351033,
+            "financial": 0.242287613
+        }
+    }, {
+        "iso_code": "NAM",
+        "country": "Namibia",
+        "data": {
+            "environmental": 0.047634137,
+            "social": 0.039761447,
+            "political": 0.124623556,
+            "financial": 0.237475663
+        }
+    }, {
+        "iso_code": "TJK",
+        "country": "Tanzania",
+        "data": {
+            "environmental": 0.050524992,
+            "social": 0.05951062,
+            "political": 0.118306266,
+            "financial": 0.22808354
+        }
+    }, {
+        "iso_code": "ZMB",
+        "country": "Zambia",
+        "data": {
+            "environmental": 0.042391642,
+            "social": 0.057109709,
+            "political": 0.130349111,
+            "financial": 0.208676428
+        }
+    }, {
+        "iso_code": "ZWE",
+        "country": "Zimbabwe",
+        "data": {
+            "environmental": 0.051050624,
+            "social": 0.058535746,
+            "political": 0.093162626,
+            "financial": 0.251300136
+        }
+    }, {
+        "iso_code": "ZAF",
+        "country": "South Africa",
+        "data": {
+            "environmental": 0.036113567,
+            "social": 0.015145622,
+            "political": 0.139010664,
+            "financial": 0.459245341
+        }
+    }]
+};
