8984b2481a8dae2871a9aa5983fd57de154b08ee
Pere <peroc79@gmail.com>
2020-02-10 10:48:02 +0100

    Add files via upload

---
+++ weights.js
@@ -0,0 +1,425 @@
+var index_weights=[
+    {
+        "total_features": 38,
+        "features": [
+            {
+                "code": "AGO",
+                "country": "Angola",
+                "data": {
+                    "general_index_val": 0.3741,
+                    "env_index_val": 0.0411,
+                    "social_index_val": 0.0437,
+                    "political_index_val": 0.0464,
+                    "financial_index_val": 0.2429
+                }
+            },
+            {
+                "code": "BDI",
+                "country": "Burundi",
+                "data": {
+                    "general_index_val": 0.4158,
+                    "env_index_val": 0.0313,
+                    "social_index_val": 0.0632,
+                    "political_index_val": 0.0564,
+                    "financial_index_val": 0.265
+                }
+            },
+            {
+                "code": "BEN",
+                "country": "Benin",
+                "data": {
+                    "general_index_val": 0.4947,
+                    "env_index_val": 0.0301,
+                    "social_index_val": 0.0497,
+                    "political_index_val": 0.1273,
+                    "financial_index_val": 0.2876
+                }
+            },
+            {
+                "code": "BFA",
+                "country": "Burkina Faso",
+                "data": {
+                    "general_index_val": 0.4595,
+                    "env_index_val": 0.0354,
+                    "social_index_val": 0.0607,
+                    "political_index_val": 0.1254,
+                    "financial_index_val": 0.238
+                }
+            },
+            {
+                "code": "BWA",
+                "country": "Botswana",
+                "data": {
+                    "general_index_val": 0.496,
+                    "env_index_val": 0.0414,
+                    "social_index_val": 0.0282,
+                    "political_index_val": 0.1396,
+                    "financial_index_val": 0.2868
+                }
+            },
+            {
+                "code": "CAF",
+                "country": "Central African Republic",
+                "data": {
+                    "general_index_val": 0.3769,
+                    "env_index_val": 0.0713,
+                    "social_index_val": 0.0677,
+                    "political_index_val": 0.0505,
+                    "financial_index_val": 0.1874
+                }
+            },
+            {
+                "code": "CIV",
+                "country": "Cote d'Ivoire",
+                "data": {
+                    "general_index_val": 0.3881,
+                    "env_index_val": 0.0212,
+                    "social_index_val": 0.0462,
+                    "political_index_val": 0.1161,
+                    "financial_index_val": 0.2047
+                }
+            },
+            {
+                "code": "CMR",
+                "country": "Cameroon",
+                "data": {
+                    "general_index_val": 0.4415,
+                    "env_index_val": 0.0252,
+                    "social_index_val": 0.0445,
+                    "political_index_val": 0.0971,
+                    "financial_index_val": 0.2747
+                }
+            },
+            {
+                "code": "COD",
+                "country": "Democratic Republic of the Congo",
+                "data": {
+                    "general_index_val": 0.3238,
+                    "env_index_val": 0.0529,
+                    "social_index_val": 0.0583,
+                    "political_index_val": 0.0587,
+                    "financial_index_val": 0.1539
+                }
+            },
+            {
+                "code": "COG",
+                "country": "Congo",
+                "data": {
+                    "general_index_val": 0.3626,
+                    "env_index_val": 0.0289,
+                    "social_index_val": 0.035,
+                    "political_index_val": 0.0614,
+                    "financial_index_val": 0.2373
+                }
+            },
+            {
+                "code": "ERI",
+                "country": "Eritrea",
+                "data": {
+                    "general_index_val": 0.379,
+                    "env_index_val": 0.0667,
+                    "social_index_val": 0.0492,
+                    "political_index_val": 0.0553,
+                    "financial_index_val": 0.2079
+                }
+            },
+            {
+                "code": "ETH",
+                "country": "Ethiopia",
+                "data": {
+                    "general_index_val": 0.7607,
+                    "env_index_val": 0.0484,
+                    "social_index_val": 0.0593,
+                    "political_index_val": 0.0912,
+                    "financial_index_val": 0.5617
+                }
+            },
+            {
+                "code": "GHA",
+                "country": "Ghana",
+                "data": {
+                    "general_index_val": 0.3557,
+                    "env_index_val": 0.0206,
+                    "social_index_val": 0.0296,
+                    "political_index_val": 0.1402,
+                    "financial_index_val": 0.1653
+                }
+            },
+            {
+                "code": "GIN",
+                "country": "Guinea",
+                "data": {
+                    "general_index_val": 0.3893,
+                    "env_index_val": 0.0397,
+                    "social_index_val": 0.0566,
+                    "political_index_val": 0.109,
+                    "financial_index_val": 0.184
+                }
+            },
+            {
+                "code": "KEN",
+                "country": "Kenya",
+                "data": {
+                    "general_index_val": 0.6983,
+                    "env_index_val": 0.048,
+                    "social_index_val": 0.0445,
+                    "political_index_val": 0.1193,
+                    "financial_index_val": 0.4865
+                }
+            },
+            {
+                "code": "LBR",
+                "country": "Liberia",
+                "data": {
+                    "general_index_val": 0.4203,
+                    "env_index_val": 0.0264,
+                    "social_index_val": 0.0534,
+                    "political_index_val": 0.1241,
+                    "financial_index_val": 0.2164
+                }
+            },
+            {
+                "code": "LSO",
+                "country": "Lesotho",
+                "data": {
+                    "general_index_val": 0.4628,
+                    "env_index_val": 0.0463,
+                    "social_index_val": 0.0519,
+                    "political_index_val": 0.0891,
+                    "financial_index_val": 0.2755
+                }
+            },
+            {
+                "code": "MDG",
+                "country": "Madagascar",
+                "data": {
+                    "general_index_val": 0.3747,
+                    "env_index_val": 0.071,
+                    "social_index_val": 0.0667,
+                    "political_index_val": 0.0993,
+                    "financial_index_val": 0.1377
+                }
+            },
+            {
+                "code": "MLI",
+                "country": "Mali",
+                "data": {
+                    "general_index_val": 0.4707,
+                    "env_index_val": 0.0533,
+                    "social_index_val": 0.0629,
+                    "political_index_val": 0.1294,
+                    "financial_index_val": 0.225
+                }
+            },
+            {
+                "code": "MOZ",
+                "country": "Mozambique",
+                "data": {
+                    "general_index_val": 0.4334,
+                    "env_index_val": 0.0462,
+                    "social_index_val": 0.0557,
+                    "political_index_val": 0.0955,
+                    "financial_index_val": 0.2359
+                }
+            },
+            {
+                "code": "MRT",
+                "country": "Mauritania",
+                "data": {
+                    "general_index_val": 0.3845,
+                    "env_index_val": 0.0475,
+                    "social_index_val": 0.0462,
+                    "political_index_val": 0.0624,
+                    "financial_index_val": 0.2285
+                }
+            },
+            {
+                "code": "MWI",
+                "country": "Malawi",
+                "data": {
+                    "general_index_val": 0.4095,
+                    "env_index_val": 0.0424,
+                    "social_index_val": 0.063,
+                    "political_index_val": 0.103,
+                    "financial_index_val": 0.2011
+                }
+            },
+            {
+                "code": "NAM",
+                "country": "Namibia",
+                "data": {
+                    "general_index_val": 0.4325,
+                    "env_index_val": 0.0476,
+                    "social_index_val": 0.0363,
+                    "political_index_val": 0.1322,
+                    "financial_index_val": 0.2165
+                }
+            },
+            {
+                "code": "NER",
+                "country": "Niger",
+                "data": {
+                    "general_index_val": 0.4235,
+                    "env_index_val": 0.0573,
+                    "social_index_val": 0.0704,
+                    "political_index_val": 0.0898,
+                    "financial_index_val": 0.206
+                }
+            },
+            {
+                "code": "NGA",
+                "country": "Nigeria",
+                "data": {
+                    "general_index_val": 0.4911,
+                    "env_index_val": 0.0307,
+                    "social_index_val": 0.0557,
+                    "political_index_val": 0.1189,
+                    "financial_index_val": 0.2858
+                }
+            },
+            {
+                "code": "RWA",
+                "country": "Rwanda",
+                "data": {
+                    "general_index_val": 0.5498,
+                    "env_index_val": 0.0272,
+                    "social_index_val": 0.0615,
+                    "political_index_val": 0.1673,
+                    "financial_index_val": 0.2936
+                }
+            },
+            {
+                "code": "SDN",
+                "country": "Sudan",
+                "data": {
+                    "general_index_val": 0.3892,
+                    "env_index_val": 0.0642,
+                    "social_index_val": 0.0431,
+                    "political_index_val": 0.056,
+                    "financial_index_val": 0.2259
+                }
+            },
+            {
+                "code": "SEN",
+                "country": "Senegal",
+                "data": {
+                    "general_index_val": 0.5396,
+                    "env_index_val": 0.0306,
+                    "social_index_val": 0.0471,
+                    "political_index_val": 0.151,
+                    "financial_index_val": 0.311
+                }
+            },
+            {
+                "code": "SLE",
+                "country": "Sierra Leone",
+                "data": {
+                    "general_index_val": 0.3661,
+                    "env_index_val": 0.0362,
+                    "social_index_val": 0.0653,
+                    "political_index_val": 0.1134,
+                    "financial_index_val": 0.1511
+                }
+            },
+            {
+                "code": "SOM",
+                "country": "Somalia",
+                "data": {
+                    "general_index_val": 0.4272,
+                    "env_index_val": 0.0705,
+                    "social_index_val": 0.0583,
+                    "political_index_val": 0.0369,
+                    "financial_index_val": 0.2616
+                }
+            },
+            {
+                "code": "SSD",
+                "country": "South Sudan",
+                "data": {
+                    "general_index_val": 0.346,
+                    "env_index_val": 0.0481,
+                    "social_index_val": 0.0621,
+                    "political_index_val": 0.0486,
+                    "financial_index_val": 0.1873
+                }
+            },
+            {
+                "code": "TCD",
+                "country": "Chad",
+                "data": {
+                    "general_index_val": 0.4254,
+                    "env_index_val": 0.0758,
+                    "social_index_val": 0.0741,
+                    "political_index_val": 0.0558,
+                    "financial_index_val": 0.2197
+                }
+            },
+            {
+                "code": "TGO",
+                "country": "Togo",
+                "data": {
+                    "general_index_val": 0.4599,
+                    "env_index_val": 0.0217,
+                    "social_index_val": 0.0499,
+                    "political_index_val": 0.1382,
+                    "financial_index_val": 0.2501
+                }
+            },
+            {
+                "code": "TZA",
+                "country": "United Republic of Tanzania",
+                "data": {
+                    "general_index_val": 0.4562,
+                    "env_index_val": 0.0512,
+                    "social_index_val": 0.0622,
+                    "political_index_val": 0.107,
+                    "financial_index_val": 0.2358
+                }
+            },
+            {
+                "code": "UGA",
+                "country": "Uganda",
+                "data": {
+                    "general_index_val": 0.5521,
+                    "env_index_val": 0.0295,
+                    "social_index_val": 0.0634,
+                    "political_index_val": 0.1298,
+                    "financial_index_val": 0.3294
+                }
+            },
+            {
+                "code": "ZAF",
+                "country": "South Africa",
+                "data": {
+                    "general_index_val": 0.6558,
+                    "env_index_val": 0.038,
+                    "social_index_val": 0.0152,
+                    "political_index_val": 0.1489,
+                    "financial_index_val": 0.4536
+                }
+            },
+            {
+                "code": "ZMB",
+                "country": "Zambia",
+                "data": {
+                    "general_index_val": 0.4259,
+                    "env_index_val": 0.0398,
+                    "social_index_val": 0.0536,
+                    "political_index_val": 0.1287,
+                    "financial_index_val": 0.2037
+                }
+            },
+            {
+                "code": "ZWE",
+                "country": "Zimbabwe",
+                "data": {
+                    "general_index_val": 0.4043,
+                    "env_index_val": 0.0493,
+                    "social_index_val": 0.0546,
+                    "political_index_val": 0.0879,
+                    "financial_index_val": 0.2125
+                }
+            }
+        ]
+    }
+];
